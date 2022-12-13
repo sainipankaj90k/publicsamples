ba839641369b6ff5ebbaec4021ec0cd95dfd411e
sainipankaj90k <sainipankaj90k@gmail.com>
2022-12-13 14:43:29 -0800

    Update AddStorageAccount_DeploymentScript.ps1

--- AddStorageAccount_DeploymentScript.ps1
+++ AddStorageAccount_DeploymentScript.ps1
@@ -1,6 +1,10 @@
 $DeploymentScriptOutputs = @{};
 $DeploymentScriptOutputs['text1'] = 'output1text';
 
+$DeploymentScriptOutputs['UserName'] = ${Env:UserName};
+$DeploymentScriptOutputs['StorageName'] = ${Env:StorageName};
+$DeploymentScriptOutputs['StorageName2'] = ${Env:StorageName2};
+
 $abc = New-AzStorageAccount -ResourceGroupName ${Env:UserName} -Name ${Env:StorageName} -Location westus -SkuName Standard_GRS;
 $DeploymentScriptOutputs['text2'] = $abc;
 
