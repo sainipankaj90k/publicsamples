61864617ddbcb10ad905bbb773db7e7912723392
sainipankaj90k <sainipankaj90k@gmail.com>
2022-12-13 20:24:59 -0800

    Create AddStorageAccount_DeploymentScriptV3.ps1

---
+++ AddStorageAccount_DeploymentScriptV3.ps1
@@ -0,0 +1,25 @@
+$DeploymentScriptOutputs = @{};
+
+$DeploymentScriptOutputs['ResourceGroupName'] = ${Env:ResourceGroupName};
+$DeploymentScriptOutputs['StorageName'] = ${Env:StorageName};
+$DeploymentScriptOutputs['KeyVaultName'] = ${Env:KeyVaultName};
+$DeploymentScriptOutputs['Location'] = ${Env:Location};
+
+$azCtx = Get-AzContext
+$azCtx
+
+$subId = $azCtx.Subscription.Id
+
+Write-Host $subId
+
+$DeploymentScriptOutputs['subId'] = $subId;
+
+Select-AzSubscription -Subscription $subId
+
+$storageAccountDetail = New-AzStorageAccount -ResourceGroupName ${Env:ResourceGroupName} -Name ${Env:StorageName} -Location westus -SkuName Standard_GRS;
+$DeploymentScriptOutputs['storageAccountDetail'] = $storageAccountDetail;
+
+
+$storageAccountDetail = New-AzKeyVault -ResourceGroupName ${Env:ResourceGroupName} -Name ${Env:KeyVaultName} -Location westus;
+$DeploymentScriptOutputs['storageAccountDetail'] = $storageAccountDetail;
+
