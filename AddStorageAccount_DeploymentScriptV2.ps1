e32c365a62b34fdeddbf1aca41c61653c12e3a46
sainipankaj90k <sainipankaj90k@gmail.com>
2022-12-13 19:55:28 -0800

    Create AddStorageAccount_DeploymentScriptV2.ps1

---
+++ AddStorageAccount_DeploymentScriptV2.ps1
@@ -0,0 +1,18 @@
+$DeploymentScriptOutputs = @{};
+$DeploymentScriptOutputs['text1'] = 'output1text';
+
+$DeploymentScriptOutputs['UserName'] = ${Env:UserName};
+$DeploymentScriptOutputs['StorageName'] = ${Env:StorageName};
+$DeploymentScriptOutputs['StorageName2'] = ${Env:StorageName2};
+
+$azCtx = Get-AzContext
+$azCtx
+
+$subId = $azCtx.Subscription.Id
+
+Write-Host $subId
+
+Select-AzSubscription -Subscription $subId
+
+$abc = New-AzStorageAccount -ResourceGroupName ${Env:UserName} -Name ${Env:StorageName} -Location westus -SkuName Standard_GRS;
+$DeploymentScriptOutputs['text2'] = $abc;
