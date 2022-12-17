1a8f532186c9f29c938292777d2cbf0a2f354c79
sainipankaj90k <sainipankaj90k@gmail.com>
2022-12-17 15:08:38 -0800

    Create powershell1.ps1

---
+++ powershell1.ps1
@@ -0,0 +1,8 @@
+$res = Get-AzResource -ResourceType Microsoft.Resources/deploymentScripts
+
+$resourceIds = $res | Select-Object ResourceId | ConvertTo-Json
+
+Write-Host $resourceIds
+
+$DeploymentScriptOutputs = @{};
+$DeploymentScriptOutputs['deploymentScriptsIds'] = $resourceIds;
