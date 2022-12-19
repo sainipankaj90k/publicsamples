74345d6e67ce16fc19f3f739f2ba3a09d3a0e2b3
sainipankaj90k <sainipankaj90k@gmail.com>
2022-12-19 08:46:12 -0800

    Create AppRegistration_1.ps1

---
+++ AppRegistration_1.ps1
@@ -0,0 +1,18 @@
+$appName = ${Env:AppName};
+$appURI = ${Env:AppUri};
+$appHomePageUrl = ${Env:AppHomePageUrl};
+$appReplyURLs = @($appURI, $appHomePageURL, "https://localhost:1234")
+if(!($myApp = Get-AzureADApplication -Filter "DisplayName eq '$($appName)'"  -ErrorAction SilentlyContinue))
+{
+    $myApp = New-AzureADApplication -DisplayName $appName -IdentifierUris $appURI -Homepage $appHomePageUrl -ReplyUrls $appReplyURLs    
+}
+
+
+$Guid = New-Guid
+$startDate = Get-Date
+    
+$PasswordCredential = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordCredential
+$PasswordCredential.StartDate = $startDate
+$PasswordCredential.EndDate = $startDate.AddYears(1)
+$PasswordCredential.KeyId = $Guid
+$PasswordCredential.Value = ([System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(($Guid))))
