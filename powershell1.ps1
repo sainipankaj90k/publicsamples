de7aca8d1699b74956e33572a81e73f53abb41ee
sainipankaj90k <sainipankaj90k@gmail.com>
2023-01-06 18:14:23 -0800

    Update powershell1.ps1

--- powershell1.ps1
+++ powershell1.ps1
@@ -6,3 +6,11 @@ Write-Host $resourceIds
 
 $DeploymentScriptOutputs = @{};
 $DeploymentScriptOutputs['deploymentScriptsIds'] = $resourceIds;
+
+$res2 = Get-AzResource -ResourceType Microsoft.ManagedIdentity/userAssignedIdentities
+
+$resourceIds = $res2 | Select-Object ResourceId | ConvertTo-Json
+
+Write-Host $resourceIds
+
+$DeploymentScriptOutputs['deploymentScriptsIds2'] = $resourceIds;
